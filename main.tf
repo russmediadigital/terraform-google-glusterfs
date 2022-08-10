1128be13277b1fbb57bb739c6a9c8f183b06f6a0
Julian Martin <julianmarti96n@gmail.com>
2022-08-10 15:31:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "google_compute_instance" "default" {
   name         = "${var.server_prefix}-${count.index}"
   machine_type = var.machine_type
   zone         = "${var.region}-${var.zones[count.index]}"
+  project      = var.project
 
   allow_stopping_for_update = true
 
@@ -29,8 +30,9 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
-    subnetwork = var.subnetwork
-    network_ip = cidrhost(var.subnet_mask, -count.index - var.ip_offset) # This takes last 3 IPs of the subnet (they are usually free - if not ip_offset can be uset to shift ips)
+    subnetwork         = var.subnetwork
+    network_ip         = cidrhost(var.subnet_mask, -count.index - var.ip_offset) # This takes last 3 IPs of the subnet (they are usually free - if not ip_offset can be uset to shift ips)
+    subnetwork_project = var.project
 
     access_config {
       // Ephemeral IP
@@ -43,16 +45,16 @@ resource "google_compute_instance" "default" {
 
   metadata_startup_script = element(
     formatlist("%v", templatefile(
-  "${path.module}/files/glusterfs_provision_server.sh",
-  {
-      cluster_size    = var.cluster_size
-      server_prefix   = var.server_prefix
-      volume_names    = join(" ", var.volume_names)
-      group           = var.group
-      user            = var.user
-      replicas_number = var.replicas_number
-  }
-)),
+      "${path.module}/files/glusterfs_provision_server.sh",
+      {
+        cluster_size    = var.cluster_size
+        server_prefix   = var.server_prefix
+        volume_names    = join(" ", var.volume_names)
+        group           = var.group
+        user            = var.user
+        replicas_number = var.replicas_number
+      }
+    )),
     count.index,
   )
 
@@ -67,10 +69,11 @@ resource "google_compute_instance" "default" {
 }
 
 resource "google_compute_disk" "default" {
-  count = var.cluster_size
-  name  = "${var.data_disk_prefix}-${count.index}"
-  type  = var.data_disk_type
-  zone  = "${var.region}-${var.zones[count.index]}"
+  project = var.project
+  count   = var.cluster_size
+  name    = "${var.data_disk_prefix}-${count.index}"
+  type    = var.data_disk_type
+  zone    = "${var.region}-${var.zones[count.index]}"
 
   size     = var.data_disk_size
   snapshot = var.data_disk_snapshot
@@ -80,23 +83,9 @@ resource "google_compute_disk" "default" {
   }
 }
 
-locals {
-  kubernetes_endpoints = templatefile("${path.module}/files/kubernetes_endpoints.json.tpl", 
-  {
-    endpoints = join(",\n    ", 
-    templatefile("${path.module}/files/endpoint.json.tpl", {
-    ip = element(
-      google_compute_instance.default.*.network_interface.0.network_ip,
-      count.index,
-    )
-  }))
-    kubernetes_endpoint_name = var.kubernetes_endpoint_name
-  }
-  )
-}
-
 resource "google_compute_subnetwork" "default" {
   name          = var.subnetwork
+  project       = var.project
   ip_cidr_range = var.subnet_mask
   region        = var.region
   network       = var.network
@@ -105,6 +94,7 @@ resource "google_compute_subnetwork" "default" {
 resource "google_compute_firewall" "default" {
   name    = "gluster-firewall"
   network = var.network
+  project = var.project
 
   allow {
     protocol = "icmp"
